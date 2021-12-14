15956bf4ddc44985adb1dab14a9a2bb6ba04cbe8
Pei-Yi Lin <pei-yi.lin@heapanalytics.com>
2021-12-14 12:50:36 -0800

    Day 7 part 1

---
+++ 7-2.py
@@ -0,0 +1,22 @@
+def getCrabPositions(filename):
+  with open(filename) as inputFile:
+    crabPositions = inputFile.readline().split(',')
+    return [int(crab) for crab in crabPositions]
+
+def getFuelToAlignTo(crabPositions, positionToAlignTo):
+  # Update this method
+  totalFuel = 0
+  for crabPosition in crabPositions:
+    totalFuel += abs(crabPosition - positionToAlignTo)
+  return totalFuel
+
+crabPositions = getCrabPositions('7-input.txt')
+minPosition = min(crabPositions)
+maxPosition = max(crabPositions)
+
+minFuel = getFuelToAlignTo(crabPositions, minPosition)
+for position in range(minPosition, maxPosition):
+  fuelForThisPosition = getFuelToAlignTo(crabPositions, position)
+  if fuelForThisPosition < minFuel:
+    minFuel = fuelForThisPosition
+print(minFuel)
